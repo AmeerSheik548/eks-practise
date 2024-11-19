2c0c85dfcb05a631da6be74d2db48c056bfbbd30
unknown <asheik@zuralabs.com>
2024-11-19 12:50:50 +0530

    initial-dev

---
+++ src/main/java/com/zura/eks/EksPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.zura.eks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EksPractiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zura/eks/EksPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zura.eks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
